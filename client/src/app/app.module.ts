cbb80a25adfa553a6694bf53fbfe30f196ac4a66
Kevin Ly <Kevin.Ly@hpa.hamburg.de>
2024-02-07 12:34:20 +0100

    Revert "section 5 (half)"
    This reverts commit cc96498831daeed19d9a5c00c02b1e2cb07546e1.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,28 +1,26 @@
 import { NgModule } from '@angular/core';
-import {
-  BrowserModule,
-  provideClientHydration,
-} from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     NgbModule,
-    BrowserAnimationsModule,
-    FormsModule
+    BrowserAnimationsModule
   ],
-  providers: [provideClientHydration()],
-  bootstrap: [AppComponent],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
