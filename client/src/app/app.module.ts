24919542bd87b5cb3b0c44d9d30a8dfac7243fde
Kevin Ly <152855969+KevinLyHH@users.noreply.github.com>
2024-02-20 14:08:50 +0100

    half way of section 11 (before photo uploader)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,17 +17,18 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorComponent } from './errors/test-error/test-error.component';
-import { ErrorInterceptor } from './_interceptor/_interceptor/error.interceptor';
+import { ErrorInterceptor } from './_interceptor/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptor/jwt.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
-import { LoadingInterceptor } from './_interceptors/loading.interceptor';
+import { LoadingInterceptor } from './_interceptor/loading.interceptor';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent, MemberListComponent, ListsComponent, MessagesComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, MemberCardComponent, MemberEditComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent, MemberListComponent, ListsComponent, MessagesComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, MemberCardComponent, MemberEditComponent, PhotoEditorComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
